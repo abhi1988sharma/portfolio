5caaafc8f35341ec18025d1e7a4c9e8d79ef5793
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-11-15 12:31:29 +0200

    update

--- src/Components/navbar/navbar.tsx
+++ src/Components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./navbar.module.scss"
 
 
@@ -9,9 +10,9 @@ type Navbarprops = {
 const Navbar: React.FC<Navbarprops> = () => {
   return (
     <div className={styles.container}>
-   <Link to="/">Home</Link> <Link to="first">My First Component</Link>
-   <Link to="/">Home</Link> <Link to="first">My First Component</Link>
-   <Link to="/">Home</Link> <Link to="first">My First Component</Link>
+      <Link to="/">Home</Link> <Link to="first">My First Component</Link>
+      <Link to="/">Home</Link> <Link to="first">My First Component</Link>
+      <Link to="/">Home</Link> <Link to="first">My First Component</Link>
   </div>
   )
 }
