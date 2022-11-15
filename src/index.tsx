b65fdec6ad159f60ad6911d71338f6544eef121c
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-11-15 12:36:37 +0200

    again

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
-import styles from "./index.module.scss"
+import "./index.module.scss"
 import SomeExampleComponent from './Components/example/someExampleComponent';
 import MyFirstComponent from './Components/myFirstComponent';
 import Navbar from './Components/navbar/navbar';
