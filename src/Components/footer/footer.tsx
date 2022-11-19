a3c5ae2abe51761f796c6f8af3e1e5eb7e19c975
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-11-19 20:56:14 +0200

    update

--- src/Components/footer/footer.tsx
+++ src/Components/footer/footer.tsx
@@ -3,10 +3,10 @@ import styles from "./footer.module.scss";
 
 const Footer: React.FC=() =>{return(
     <div className={styles.container}>
-    <a className={styles.footerLinks} href="https://www.linkedin.com/">LinkedIn</a>
-    <a className={styles.footerLinks} href="https://github.com/abhi1988sharma">Github</a>
-    <a className={styles.footerLinks} href="https://twitter.com/">Twitter</a>
-    <a className={styles.footerLinks} href="https://abhi1988sharma.github.io/portfolio">Website</a>
+    <a className={styles.footerLinks} href="https://www.linkedin.com/"><img className={styles.mediaLogo} src="images/linkedin.png" alt= "linkedin"></img></a>
+    <a className={styles.footerLinks} href="https://github.com/abhi1988sharma"><img className={styles.mediaLogo} src="images/github.png" alt= "github"></img></a>
+    <a className={styles.footerLinks} href="https://twitter.com/"><img className={styles.mediaLogo} src="images/twitter.png" alt= "twitter"></img></a>
+    <a className={styles.footerLinks} href="https://abhi1988sharma.github.io/portfolio"><img className={styles.mediaLogo} src="images/web-link.png" alt= "Webpage"></img></a>
     <p>© Abhishek Sharma.</p>
   </div>
 );}
