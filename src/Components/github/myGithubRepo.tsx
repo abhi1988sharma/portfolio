97ec9630b7327c55700dbf28564d0ec803ea3804
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-12-08 22:24:10 +0200

    styling corrections

--- src/Components/github/myGithubRepo.tsx
+++ src/Components/github/myGithubRepo.tsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { IRepo } from './githubData'
 import styles from "./githubData.module.scss";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 
 const githubEndpoint = "https://api.github.com/users/abhi1988sharma/repos"
 
 const MyGithubRepos: React.FC = () => {
 
- 
+  
 
   const [githubData, setGithubData] = useState<IRepo[]>([])
 
@@ -22,45 +29,51 @@ const MyGithubRepos: React.FC = () => {
     <><h2 style = {{
 fontSize: '50px',
 color: 'rgb(27, 96, 111)',
-
 justifyContent: 'center',
 textAlign: 'center'
 
-}}>
+}}> My github projects</h2>
+  <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    rowGap: '60px',
+    columnGap: '60px',
+    marginTop: '60px', 
+    marginBottom: 'auto',
+    paddingBottom: '60px'
+    
 
-
-  My github projects</h2><div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      rowGap: '60px',
-      columnGap: '60px',
-      marginTop: '60px',
-      marginBottom: 'auto',
-      paddingBottom: '60px',
-    }}>
-
+    
+  }}>
 
       {githubData ? githubData.map(x => (
-        <div key={x.id} style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: 280,
-          width: 300,
-          background: 'rgb(27, 96, 111)',
-          color: 'rgb(194, 237, 247)',
-          padding: '10px',
-          margin: '10px',
-          //justifyContent: 'center',
-          textAlign: 'center',
-          borderRadius: '10%',
-        }}>
+        <div key={x.id}  >
+          <Card sx={{ maxWidth: 365 }} style={{background: 'rgb(27, 96, 111)'}}>
+      <CardMedia
+        component="img"
+        height="80"
+        image="/images/projectBack.gif"
+        alt="Projects Pic"
+      />
+      <CardContent >
+        <Typography gutterBottom variant="h5" component="div" style={{color: 'rgb(194, 237, 247)'}}>
+        {x.name}
+        </Typography>
+        <Typography variant="body2" style={{
+        color: 'rgb(194, 237, 247)'}} >
+        {x.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small"><a href={x.html_url}><img className={styles.gitLogo} src="images/github.png" alt="github"></img></a></Button>
+        
+      </CardActions>
+    </Card>
 
-          <h3 className={styles.projectName}>{x.name}</h3>
-          <p className={styles.description}>{x.description}</p>
-          <a href={x.html_url}><img className={styles.gitLogo} src="images/github.png" alt="github"></img></a>
 
+      
 
         </div>
       ))
